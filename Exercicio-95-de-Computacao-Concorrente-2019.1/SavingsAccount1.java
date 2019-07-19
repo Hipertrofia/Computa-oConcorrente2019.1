2751ed77fea6709f07e8ff9fb8b942f8b12aa15b
Hipertrofia <48809869+Hipertrofia@users.noreply.github.com>
2019-07-19 16:42:41 -0300

    Account1

--- Exercicio-95-de-Computacao-Concorrente-2019.1/SavingsAccount1.java
+++ Exercicio-95-de-Computacao-Concorrente-2019.1/SavingsAccount1.java
@@ -1,1 +1,38 @@
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
 
+public class SavingsAccount1 {
+    int saldo;
+    ReentrantLock lock = new ReentrantLock();
+	Condition saldoCond = lock.newCondition();
+
+    SavingsAccount1(int saldo) {
+        this.saldo = saldo;
+    }
+
+    int getSaldo() {
+        return this.saldo;
+    }
+
+    void deposito(int valor) {
+        try {
+            lock.lock();
+            this.saldo += valor;
+            saldoCond.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    void saque(int valor) throws InterruptedException {
+        try {
+            lock.lock();
+            while(this.saldo < valor) {
+                saldoCond.await();
+            }
+            this.saldo -= valor;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
